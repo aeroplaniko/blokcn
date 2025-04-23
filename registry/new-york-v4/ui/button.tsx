acb853f7a94ce58b130329a5d0b8f69b485f1298
spm_sitecore <spyros.misichronis@sitecore.com>
2025-04-23 17:09:03 +0300

    update components

--- registry/new-york-v4/ui/button.tsx
+++ registry/new-york-v4/ui/button.tsx
@@ -22,9 +22,10 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 px-6 has-[>svg]:px-4",
+        default: "h-10 min-w-10 px-4 py-2 has-[>svg]:px-4",
+        sm: "h-8 min-w-8 gap-1.5 px-3 has-[>svg]:px-2.5",
+        lg: "h-12 min-w-12 px-6 has-[>svg]:px-4",
+        xs: "h-6 min-w-6 px-2 has-[>svg]:px-2",
         icon: "size-9",
       },
     },
